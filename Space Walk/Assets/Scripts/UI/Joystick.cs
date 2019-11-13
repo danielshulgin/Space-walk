9c82d9556c25047503bf8ba5454e5691bb15195b
danielshulgin <danielshulgin@users.noreply.github.com>
2019-11-13 20:20:05 +0200

    refactoring naming

--- Space Walk/Assets/Scripts/UI/Joystick.cs
+++ Space Walk/Assets/Scripts/UI/Joystick.cs
@@ -12,91 +12,94 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 
     public float HandleRange
     {
-        get { return handleRange; }
-        set { handleRange = Mathf.Abs(value); }
+        get => _handleRange;
+        set => _handleRange = Mathf.Abs(value);
     }
 
     public float DeadZone
     {
-        get { return deadZone; }
-        set { deadZone = Mathf.Abs(value); }
+        get => _deadZone;
+        set => _deadZone = Mathf.Abs(value);
     }
 
 
     [SerializeField] 
-    private float handleRange = 1;
+    private float _handleRange = 1;
     [SerializeField] 
-    private float deadZone = 0;
+    private float _deadZone = 0;
 
+    [SerializeField]
+    private RectTransform _background;
     [SerializeField] 
-    protected RectTransform background = null;
-    [SerializeField] 
-    private RectTransform handle = null;
+    private RectTransform _handle;
 
     private Canvas _canvas;
     private Camera _cam;
 
-    private Vector2 input = Vector2.zero;
+    private Vector2 _input = Vector2.zero;
+    private bool _stay;
 
     protected virtual void Start()
     {
-        HandleRange = handleRange;
-        DeadZone = deadZone;
+        HandleRange = _handleRange;
+        DeadZone = _deadZone;
         _canvas = GetComponentInParent<Canvas>();
         if (_canvas == null)
             Debug.LogError("The Joystick is not placed inside a canvas");
 
         Vector2 center = new Vector2(0.5f, 0.5f);
-        background.pivot = center;
-        handle.anchorMin = center;
-        handle.anchorMax = center;
-        handle.pivot = center;
-        handle.anchoredPosition = Vector2.zero;
+        _background.pivot = center;
+        _handle.anchorMin = center;
+        _handle.anchorMax = center;
+        _handle.pivot = center;
+        _handle.anchoredPosition = Vector2.zero;
         _cam = _canvas.worldCamera;
     }
 
-    public virtual void OnPointerDown(PointerEventData eventData)
-    {
-        OnDrag(eventData);
-        stay = true;
-    }
-
-    bool stay;
     private void Update()
     {
-        if (stay)
+        if (_stay)
         {
-            OnMove(input);
+            OnMove(_input);
         }
     }
 
+    //interface implementations IPointerDownHandler
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+        _stay = true;
+    }
+
+    //interface implementations IDragHandler
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 position = RectTransformUtility.WorldToScreenPoint(_cam, background.position);
-        Vector2 radius = background.sizeDelta / 2;
-        input = (eventData.position - position) / (radius * _canvas.scaleFactor);
-        HandleInput(input.magnitude, input.normalized);
-        handle.anchoredPosition = input * radius * handleRange;
+        Vector2 position = RectTransformUtility.WorldToScreenPoint(_cam, _background.position);
+        Vector2 radius = _background.sizeDelta / 2;
+        _input = (eventData.position - position) / (radius * _canvas.scaleFactor);
+        HandleInput(_input.magnitude, _input.normalized);
+        _handle.anchoredPosition = _input * radius * _handleRange;
     }
 
     protected virtual void HandleInput(float magnitude, Vector2 normalised)
     {
-        if (magnitude > deadZone)
+        if (magnitude > _deadZone)
         {
             if (magnitude > 1)
-                input = normalised;
+                _input = normalised;
              OnMove(normalised);
         }
         else
         {
-            input = Vector2.zero;
+            _input = Vector2.zero;
         }
     }
 
+    //interface implementations IPointerUpHandler
     public virtual void OnPointerUp(PointerEventData eventData)
     {
-        input = Vector2.zero;
-        handle.anchoredPosition = Vector2.zero;
-        stay = false;
+        _input = Vector2.zero;
+        _handle.anchoredPosition = Vector2.zero;
+        _stay = false;
     }
 }
